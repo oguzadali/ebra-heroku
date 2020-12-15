d26c6736836ad9452d2d56e311637abd2aa332a0
oguzadali <ouzadali1@gmail.com>
2020-12-15 17:15:47 +0300

    Added a Procfile.

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const path = require('path');//used for connect html in express
+const favicon = require('serve-favicon'); //used for fav.ico
+
+const morgan = require('morgan')
+
+// const routes = require("./routes/index-route");
+const app = express()
+const port = 7800;
+
+
+
+
+app.use(express.static("public"))
+app.use(favicon(path.join(__dirname, "./public/favicon.ico")))
+app.use(morgan('tiny'))
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/main.html'));
+})
+app.get('/article', function (req, res) {
+    res.sendFile(path.join(__dirname + '/article.html'));
+})
+// app.use("/", routes)
+
+app.listen(port, () => {
+    console.log(`Blog app listening at \x1b[31mhttp://localhost:${port}\x1b[0m`)
+})
